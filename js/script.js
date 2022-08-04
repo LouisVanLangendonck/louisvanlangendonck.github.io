9df58f168be1bd91e5ef99fba4529f6ac12c5272
Louis Van Langendonck <louisvanlangendonck@hotmail.com>
2022-08-05 01:01:08 +0200

    tree_optimized

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ const width = canvas.width = window.innerWidth;
 const height = canvas.height = window.innerHeight;
 const ctx = canvas.getContext('2d');
 
+const start_width = 50;
+
 class Branch {
     constructor(x,y,width,angle){
         this.x = x;
@@ -10,8 +12,8 @@ class Branch {
         this.width = width;
         this.angle = angle;
         // this.livelihood = livelihood;
-        ctx.strokeStyle = 'white'
-        ctx.fillStyle = 'black';
+        ctx.strokeStyle = 'black';
+        ctx.fillStyle = 'white';
     }
 
     update(){
@@ -48,6 +50,20 @@ class Branch {
     }
 }
 
-const branch = new Branch(300,0,50,Math.PI/2);
+let branch;
+
+if (Math.random() < 0.5){
+    if (Math.random() < 0.5){
+        branch = new Branch(width/3 + Math.random()*width*(2/3),0,start_width,Math.PI/2);
+    }
+    else {
+        branch = new Branch(width/3 + Math.random()*width*(2/3),height,start_width,Math.PI*(3/2));
+    }
+}
+
+else {
+        branch = new Branch(width,Math.random()*height,start_width,Math.PI);
+}
+
 branch.update();
 
--- js/script_dubbele_lijn.js
+++ js/script_dubbele_lijn.js
@@ -4,6 +4,7 @@ const height = canvas.height = window.innerHeight;
 const ctx = canvas.getContext('2d');
 
 
+
 class Root {
     constructor(x,y,x2,y2,angle,livelihood,curvability){
         this.x_1 = x;
